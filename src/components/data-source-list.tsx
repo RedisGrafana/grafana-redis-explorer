1b492e180a2dd789d9d39b84412c90c98fb2aee1
Mikhail <mikhail@redislabs.com>
2020-10-29 05:20:01 -0700

    List Data Source on Root page

---
+++ src/components/data-source-list.tsx
@@ -0,0 +1,86 @@
+import { RedisEnterpriseSoftware } from 'icons';
+import React, { FC } from 'react';
+import { Container, HorizontalGroup, InfoBox, LinkButton, VerticalGroup } from '@grafana/ui';
+
+/**
+ * Properties
+ */
+interface Props {
+  /**
+   * Data sources
+   *
+   * @type {any[]}
+   */
+  datasources?: any[];
+}
+
+export const DataSourceList: FC<Props> = ({ datasources }) => {
+  /**
+   * Check if any data sources was added
+   */
+  if (datasources?.length === 0) {
+    return (
+      <div>
+        <div className="page-action-bar">
+          <div className="page-action-bar__spacer" />
+          <LinkButton href="datasources/new" icon="database">
+            Add Redis Enterprise Data Source
+          </LinkButton>
+        </div>
+        <InfoBox title="Please add Redis Enterprise Data Sources.">
+          <p>You can add as many data sources as you want to support multiple Redis Enterprise clusters.</p>
+        </InfoBox>
+      </div>
+    );
+  }
+
+  /**
+   * Return
+   */
+  return (
+    <div>
+      <div className="page-action-bar">
+        <div className="page-action-bar__spacer" />
+        <LinkButton href="datasources/new" icon="database">
+          Add Redis Enterprise Data Source
+        </LinkButton>
+      </div>
+
+      <section className="card-section card-list-layout-list">
+        <ol className="card-list">
+          {datasources?.map((redis, index) => {
+            const title = redis.fields?.name ? 'Working as expected' : "Can't connect";
+            const fill = redis.fields?.name ? '#5B62F5' : '#A7A7A7';
+
+            console.log(redis);
+            return (
+              <li className="card-item-wrapper" key={index} aria-label="check-card">
+                <a className="card-item" href={`datasources/edit/${redis.id}`}>
+                  <HorizontalGroup justify="space-between">
+                    <HorizontalGroup justify="flex-start">
+                      <Container margin="xs">
+                        <RedisEnterpriseSoftware size={32} fill={fill} title={title} />
+                      </Container>
+                      <VerticalGroup>
+                        <div className="card-item-name">{redis.name}</div>
+                        <div className="card-item-sub-name">{redis.jsonData?.host}</div>
+                      </VerticalGroup>
+                    </HorizontalGroup>
+
+                    <HorizontalGroup justify="flex-end">
+                      {!redis.commands?.length && (
+                        <div className="card-item-header">
+                          <div className="card-item-type">{redis.fields?.name}</div>
+                        </div>
+                      )}
+                    </HorizontalGroup>
+                  </HorizontalGroup>
+                </a>
+              </li>
+            );
+          })}
+        </ol>
+      </section>
+    </div>
+  );
+};
--- src/components/root-page.tsx
+++ src/components/root-page.tsx
@@ -1,6 +1,19 @@
 import React, { PureComponent } from 'react';
-import { AppRootProps } from '@grafana/data';
-import { GlobalSettings } from '../types';
+import { QueryTypeValue } from 'redis-enterprise-datasource/api';
+import { REQuery } from 'redis-enterprise-datasource/types';
+import {
+  AppRootProps,
+  DataQueryRequest,
+  DataQueryResponse,
+  DataQueryResponseData,
+  Field,
+  NavModelItem,
+} from '@grafana/data';
+import { getBackendSrv, getDataSourceSrv } from '@grafana/runtime';
+import { InfoBox } from '@grafana/ui';
+import { DataSourceType, GlobalSettings } from '../types';
+import { DataSourceList } from './data-source-list';
+import { head } from 'lodash';
 
 /**
  * Properties
@@ -10,25 +23,148 @@ interface Props extends AppRootProps<GlobalSettings> {}
 /**
  * State
  */
-interface State {}
+interface State {
+  /**
+   * Data sources
+   *
+   * @type {any[]}
+   */
+  datasources?: any[];
+
+  /**
+   * Loading
+   *
+   * @type {boolean}
+   */
+  loading: boolean;
+}
 
 /**
  * Root Page
  */
 export class RootPage extends PureComponent<Props, State> {
   /**
-   * Constructor
-   *
-   * @param {Props} props Properties
+   * Default state
    */
-  constructor(props: Props) {
-    super(props);
+  state: State = {
+    loading: true,
+  };
+
+  /**
+   * Mount
+   */
+  async componentDidMount() {
+    this.updateNav();
+
+    /**
+     * Get data sources
+     */
+    const datasources = await getBackendSrv()
+      .get('/api/datasources')
+      .then((result: any) => {
+        return result.filter((ds: any) => {
+          return ds.type === DataSourceType.SOFTWARE;
+        });
+      });
+
+    /**
+     * Check supported commands for Redis Data Sources
+     */
+    await Promise.all(
+      datasources.map(async (ds: any) => {
+        ds.fields = {};
+
+        /**
+         * Get Data Source
+         */
+        const redis = await getDataSourceSrv().get(ds.name);
+
+        /**
+         * Execute query
+         */
+        const query = (redis.query({
+          targets: [{ queryType: QueryTypeValue.CLUSTER }],
+        } as DataQueryRequest<REQuery>) as unknown) as Promise<DataQueryResponse>;
+
+        /**
+         * Get available commands
+         */
+        await query
+          .then((response: DataQueryResponse) => response.data)
+          .then((data: DataQueryResponseData[]) =>
+            data.forEach((item: DataQueryResponseData) => {
+              item.fields.forEach((field: Field) => {
+                ds.fields[field.name] = head(field.values.toArray());
+              });
+            })
+          );
+      })
+    );
+
+    /**
+     * Set state
+     */
+    this.setState({
+      datasources,
+      loading: false,
+    });
+  }
+
+  /**
+   * Navigation
+   */
+  updateNav() {
+    const { path, onNavChanged, meta } = this.props;
+    const tabs: NavModelItem[] = [];
+
+    /**
+     * Home
+     */
+    tabs.push({
+      text: 'Home',
+      url: path,
+      id: 'home',
+      icon: 'fa fa-fw fa-sitemap',
+      active: true,
+    });
+
+    /**
+     * Header
+     */
+    const node = {
+      text: 'Redis Enterprise Application',
+      img: meta.info.logos.large,
+      subTitle: 'Redis Enterprise Data Source',
+      url: path,
+      children: tabs,
+    };
+
+    /**
+     * Update the page header
+     */
+    onNavChanged({
+      node: node,
+      main: node,
+    });
   }
 
   /**
    * Render
    */
   render() {
-    return <div>Work in progress...</div>;
+    const { loading, datasources } = this.state;
+
+    /**
+     * Loading
+     */
+    if (loading) {
+      return (
+        <InfoBox title="Loading...">
+          <p>Loading time depends on the number of configured data sources.</p>
+        </InfoBox>
+      );
+    }
+
+    return <DataSourceList datasources={datasources} />;
   }
 }
