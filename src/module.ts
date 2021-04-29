8015e7df1493c5f81c026c9437834cf217f4b259
Mikhail <47795110+mikhailredis@users.noreply.github.com>
2021-04-29 15:10:21 -0400

    Remove databases config page

--- src/module.ts
+++ src/module.ts
@@ -1,19 +1,12 @@
 import { AppPlugin } from '@grafana/data';
-import { Config, RootPage, ClusterDatabases } from './components';
+import { Config, RootPage } from './components';
 import { GlobalSettings } from './types';
 
 /**
  * Application Plug-in
  */
-export const plugin = new AppPlugin<GlobalSettings>()
-  .setRootPage(RootPage)
-  .addConfigPage({
-    title: 'Config',
-    body: Config,
-    id: 'config',
-  })
-  .addConfigPage({
-    title: 'Cluster databases',
-    body: ClusterDatabases as any,
-    id: 'cluster-databases',
-  });
+export const plugin = new AppPlugin<GlobalSettings>().setRootPage(RootPage).addConfigPage({
+  title: 'Config',
+  body: Config,
+  id: 'config',
+});
